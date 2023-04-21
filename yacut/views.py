387178bbaed74907525f574a07cc4ec7fef1d02c
Никита Федяев <itsfedos@gmail.com>
2023-04-21 21:49:42 +0300

    templates, views

--- yacut/views.py
+++ yacut/views.py
@@ -1,18 +1,32 @@
 import random
 import string
 
-from flask import render_template
+from flask import abort, flash, redirect, render_template, url_for
 
 from settings import URL_POSTFIX_SIZE
-from yacut import app
+from yacut import app, db
 from yacut.forms import URLMapForm
 from yacut.models import URLMap
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def create_shortlink():
-    # url = URLMapForm()
-    return render_template('index.html')
+    form = URLMapForm()
+    if form.validate_on_submit():
+        url = URLMap(
+            original=form.original.data,
+            short=form.short.data,
+        )
+        db.session.add(url)
+        db.session.commit()
+        return redirect(url_for('url_view', id=url.id))
+    return render_template('index.html', form=form)
+
+
+@app.route('/url/<int:id>/')
+def url_view(id):
+    url_link = URLMap.query.get_or_404(id)
+    return render_template('url_view.html', url_link=url_link)
 
 
 def get_unique_short_id():
