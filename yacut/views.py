2fd1f40162f206aaea103dfc836440a79594b1c7
Никита Федяев <itsfedos@gmail.com>
2023-04-21 16:58:42 +0300

    templates update

--- yacut/views.py
+++ yacut/views.py
@@ -1,6 +1,8 @@
 import random
 import string
 
+from flask import render_template
+
 from settings import URL_POSTFIX_SIZE
 from yacut import app
 from yacut.forms import URLMapForm
@@ -9,7 +11,8 @@ from yacut.models import URLMap
 
 @app.route('/')
 def create_shortlink():
-    url = URLMapForm()
+    # url = URLMapForm()
+    return render_template('index.html')
 
 
 def get_unique_short_id():
