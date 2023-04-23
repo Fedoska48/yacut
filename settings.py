aaa5c554deab7f1f00f116ef8748fed5c3065e8f
Никита Федяев <itsfedos@gmail.com>
2023-04-24 02:37:32 +0300

    1st review

--- settings.py
+++ settings.py
@@ -21,6 +21,8 @@ API_FIELDS = {
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'SQLALCHEMY_DATABASE_URI', default='sqlite:///db.sqlite3'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
