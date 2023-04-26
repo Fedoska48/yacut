542fe9c2c9241dce501f6ac561373df19830c026
Никита Федяев <itsfedos@gmail.com>
2023-04-26 16:45:56 +0300

    forms update

--- yacut/forms.py
+++ yacut/forms.py
@@ -2,14 +2,24 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length, Optional
 
+from yacut.models import SHORT_MAX_LEN, ORIGINAL_MAX_LEN
+
+ORIGINAL_LINK_FIELD_TEXT = 'Вставьте ссылку для обработки'
+CUSTOM_ID_FIELD_TEXT = 'Вариант короткой ссылки'
+REQIURED_FIELD_TEXT = 'Обязательное поле'
+SUBMIT_TEXT = 'Создать'
+
 
 class URLMapForm(FlaskForm):
     original_link = TextAreaField(
-        'Вставьте ссылку для обработки',
-        validators=[Length(1, 512), DataRequired(message='Обязательное поле')]
+        ORIGINAL_LINK_FIELD_TEXT,
+        validators=[
+            Length(max=ORIGINAL_MAX_LEN),
+            DataRequired(message=REQIURED_FIELD_TEXT)
+        ]
     )
     custom_id = StringField(
-        'Вариант короткой ссылки',
-        validators=[Length(1, 16), Optional()]
+        CUSTOM_ID_FIELD_TEXT,
+        validators=[Length(max=SHORT_MAX_LEN), Optional()]
     )
-    submit = SubmitField('Создать')
+    submit = SubmitField(SUBMIT_TEXT)
