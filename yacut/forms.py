64fb70b113957e15ecd9bf55b6f42f5d4585c0fe
Никита Федяев <itsfedos@gmail.com>
2023-04-26 17:00:36 +0300

    forms update

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, ValidationError
 
-from yacut.models import SHORT_MAX_LEN, ORIGINAL_MAX_LEN
+from yacut.models import (ORIGINAL_MAX_LEN, SHORT_MAX_LEN, LETTERS_AND_DIGITS,
+                          PATTERN_ERROR, URLMap)
+from yacut.views import ALREADY_EXISTS_MAIN
 
 ORIGINAL_LINK_FIELD_TEXT = 'Вставьте ссылку для обработки'
 CUSTOM_ID_FIELD_TEXT = 'Вариант короткой ссылки'
@@ -23,3 +25,11 @@ class URLMapForm(FlaskForm):
         validators=[Length(max=SHORT_MAX_LEN), Optional()]
     )
     submit = SubmitField(SUBMIT_TEXT)
+
+    def validate_custom_id(self):
+        short = self.custom_id.data
+        if URLMap.get_short(short):
+            raise ValidationError(ALREADY_EXISTS_MAIN.format(short))
+        for char in short:
+            if char not in LETTERS_AND_DIGITS:
+                raise ValidationError(PATTERN_ERROR)
